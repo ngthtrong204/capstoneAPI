ed88060b9c49bfbbf6c5a361086147b3111e46d0
ngthtrong204 <ngthtrong204@gmail.com>
2023-03-01 12:39:58 +0700

    wwtf i this

---
+++ customer/controller/mainCustomer.js
@@ -0,0 +1,4 @@
+//==============Helper===============//
+function getElement(selector){
+    return document.querySelector(selector)
+}
---
+++ customer/controller/plusMinusButton.js
@@ -0,0 +1,5 @@
+// function buttonPlus(){
+//     getElement(".")
+// }
+let test = document.querySelector("div.testing > input").value
+console.log(test);
